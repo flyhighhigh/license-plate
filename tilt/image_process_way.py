b469457d0e392d84e3d35bc8d0e6d0f71e410798
flyhighhigh <galenliu910322@gmail.com>
2023-12-13 16:35:05 +0800

    video

--- tilt/image_process_way.py
+++ tilt/image_process_way.py
@@ -22,6 +22,25 @@ def rotatedDice(image, cnt):
     matting_result = image[y:y+h,x:x+w,:]
     return matting_result
 
+def yt_way(gray):
+    # bfilt = cv2.bilateralFilter(gray,11, 11, 17) # noise reduce
+    # cv2.imshow("bfilt", bfilt)
+    # edged = cv2.Canny(bfilt, 50, 200)
+    # cv2.imshow("edged", edged)
+    
+    # (points,_) = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = sorted(points, key=cv2.contourArea, reverse=True) [:100]
+    # location = None
+    # for cnt in cnts:
+    #     ap = cv2.approxPolyDP(cnt, 10, True)
+    #     if len(ap) == 4:
+    #         location = ap
+    #         break
+    # q = img.copy()
+    # cv2.drawContours(q, [location], -1, (0,0,255), 2)
+    # cv2.imshow("qq", q)
+    pass
+
 names = os.listdir("tilt_plate_my")
 for name in names:
     img = cv2.imread("tilt_plate_my\\"+name)
@@ -69,12 +88,61 @@ for name in names:
         # cv2.drawContours(output, [c], -1, (0,0,255), 2)
         # draw the biggest contour (c) in green
         
-        x,y,ww,hh = cv2.boundingRect(c)
-        cv2.rectangle(output,(x,y),(x+ww,y+hh),(0,255,0),2)
+        # x,y,ww,hh = cv2.boundingRect(c)
+        # cv2.rectangle(output,(x,y),(x+ww,y+hh),(0,255,0),2)
         
-        # ellipse = cv2.fitEllipse(c)
-        # (center, axes, angle) = ellipse
-        # cv2.ellipse(output, ellipse, (0, 255, 255), 2)
+        
+        hull = cv2.convexHull(c)
+        h_mask = np.zeros(thres.shape, np.uint8)
+        cv2.drawContours(h_mask, [hull], -1, (255,255,255), -1)
+        
+        (X, Y), (LAxis, SAxis), Angle = ellipse = cv2.fitEllipse(hull) # angle順時針 0是垂直， 90是水平，180是垂直
+        # cv2.ellipse(output,ellipse, (255,0,0), 2)
+        if LAxis < SAxis: LAxis, SAxis = SAxis, LAxis
+        
+        Angle = Angle - 90
+        rotation_matrix = cv2.getRotationMatrix2D((X,Y), Angle, 1)
+        iii = cv2.warpAffine(h_mask.copy(), rotation_matrix,(w, h))
+        iij = cv2.warpAffine(img.copy(), rotation_matrix,(w, h))
+        # iii = iii[int(Y-SAxis/2):int(Y+SAxis/2), int(X-LAxis/2):int(X+LAxis/2)]
+        cv2.rectangle(iii, (int(X-LAxis/2),int(Y-SAxis/2)), (int(X+LAxis/2),int(Y+SAxis/2)), (255, 255, 255), 2)
+        
+        print((X, Y), (LAxis, SAxis), Angle)
+        
+        # rect = cv2.RotatedRect((X, Y), (LAxis, SAxis), Angle)
+        # box = cv2.boxPoints(rect) # cv2.cv.BoxPoints(rect) for OpenCV <3.x
+        # pt0, pt1, pt2, pt3 = box.tolist()
+        # print(pt0, pt1, pt2, pt3)
+        # lpt, rpt, dpt = [], [], [0,0] # 左點 右典 下中點
+        # for i in range(2):
+        #     lpt.append(int((pt0[i] + pt3[i])/2))
+        #     rpt.append(int((pt1[i] + pt2[i])/2))
+        # cv2.circle(output, lpt, 0, (0, 0, 255), 5)
+        # cv2.circle(output, rpt, 0, (0, 0, 255), 5)
+        
+        # if Angle > 90:
+        #     dpt[0] = rpt[0]
+        #     dpt[1] = lpt[1]
+        # else:
+        #     dpt[0] = lpt[0]
+        #     dpt[1] = rpt[1]
+        # dpt = [(lpt[0]+rpt[0])//2, min(lpt[1], rpt[1])]
+            
+        # cv2.circle(output, dpt, 0, (0, 255, 0), 5)
+        
+        # vw = w//8
+        # vh = h//8
+        # cx = w//2
+        # cy = h//2
+        # dx = cx # +vw if Angle > 90 else cx-vw
+        # dy = cy-vh//2 # if Angle > 90 else 
+        # p1 = np.float32([lpt, rpt, dpt])
+        # p2 = np.float32([[cx-vw, cy], [cx+vw, cy], [dx, dy]])
+        # M = cv2.getAffineTransform(p1, p2)
+        # output2 = cv2.warpAffine(output, M, (w, h))
+        # cv2.ellipse(output, ellipse, (255,0,0), 2)
+        
+        
         
         
         
@@ -86,22 +154,31 @@ for name in names:
 
         
         
-        hull = cv2.convexHull(c)
-        cv2.drawContours(output, [hull], -1, (255,0,0), 2)
+        # hull = cv2.convexHull(c)
+        # cv2.drawContours(output, [hull], -1, (255,0,0), 2)
         
-        h_mask = np.zeros(thres.shape, np.uint8)
-        cv2.drawContours(h_mask, [hull], -1, (255,255,255), -1)
-        thres_masked = cv2.bitwise_and(thres, h_mask)
         
-        # rotated = rotatedDice(cv2.cvtColor(c_mask.copy(),cv2.COLOR_GRAY2BGR),c)
+        # h_mask = np.zeros(thres.shape, np.uint8)
+        # cv2.drawContours(h_mask, [hull], -1, (255,255,255), -1)
+        # thres_masked = cv2.bitwise_and(hist, h_mask)
+        # thres_masked += ~h_mask
+        # ret, bin_masked = cv2.threshold(thres_masked, 150, 255, cv2.THRESH_BINARY_INV)
+        # (cnts ,_) = cv2.findContours(bin_masked, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # if len(cnts) > 10:
+        #     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
         
-        c_mask = h_mask
-        dst = cv2.cornerHarris(c_mask,2,3,0.04)
-        #result is dilated for marking the corners, not important
-        dst = cv2.dilate(dst,None)
-        # Threshold for an optimal value, it may vary depending on the image.
-        output[dst>0.01*dst.max()]=[0,0,255]
-        # cv2.imshow('o',output)
+        # def greater(a):
+        #     x, y, w, h = cv2.boundingRect(c)
+        #     return x
+            
+        # cnts = sorted(cnts, key=greater)
+        # leftmost = cnts[0]
+        # rightmost = cnts[-1]
+        # nnnn_mask = np.zeros(thres.shape, np.uint8)
+        # cv2.drawContours(nnnn_mask, [leftmost], -1, (255,255,0), -1)
+        # cv2.drawContours(nnnn_mask, [rightmost], -1, (255,0,0), -1)
+        
+        
         
         
         
@@ -134,9 +211,18 @@ for name in names:
     cv2.imshow("b_thres",smaller_plz(blur_thres, 1))
     cv2.imshow("dilate",smaller_plz(dilats1, 1))
     cv2.imshow("output",smaller_plz(output, 1))
-    cv2.imshow("c_mask",smaller_plz(c_mask, 1))
+    # cv2.imshow("output2",smaller_plz(output2, 1))
+    # cv2.imshow("c_mask",smaller_plz(c_mask, 1))
     cv2.imshow("h_mask",smaller_plz(h_mask, 1))
-    cv2.imshow("thres_masked",smaller_plz(thres_masked, 1))
+    # cv2.imshow("th_masked",smaller_plz(thres_masked, 1))
+    # cv2.imshow("bin_masked",smaller_plz(bin_masked, 1))
+    # cv2.imshow("nnn_masked",smaller_plz(nnnn_mask, 1))
+    cv2.imshow("iii",smaller_plz(iii, 1))
+    cv2.imshow("iij",smaller_plz(iij, 1))
+    
+    
+    yt_way(gray)
+    # cv2.imshow("qq",qq)
     # cv2.imshow("rotated",smaller_plz(rotated, 1))
     # cv2.imshow("lines",smaller_plz(linesimg, 1))
 
